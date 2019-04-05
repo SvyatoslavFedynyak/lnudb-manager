76df8c99827594076f7da4a895b46d5dfd30d412
Svyatoslav <svyatoslav912@gmail.com>
2019-04-05 14:33:12 +0300

    Start proj

---
+++ src/main.py
@@ -0,0 +1,46 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *   
+
+class mainWindow(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):
+        
+        QToolTip.setFont(QFont('Arial', 10))
+        
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)       
+        
+        self.setLabels()
+        self.setGeometry(200, 70, 1000, 600)
+        self.setWindowTitle('Cloud Manager')  
+        self.show()
+
+    def setLabels(self):
+        
+        vmLabel = QLabel()
+        vmCountLabel = QLabel()
+        
+        
+        
+        vmLabel.setText('Virtual Machines')
+        vmLabel.setGeometry(200, 300, 100, 100)
+        vmCountLabel.setText('10')
+        vmCountLabel.setGeometry(300, 300, 100, 100)
+        
+
+if __name__ == '__main__':
+    
+    mainApp = QApplication(sys.argv)
+    mv = mainWindow()
+    
+    sys.exit(mainApp.exec_())
